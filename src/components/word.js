8d6d3c650118c34e5e34a4b990e0f7f5aab6dd38
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-06-11 23:59:17 +0300

    added new css file and methods for card

--- src/components/word.js
+++ src/components/word.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './word.css';
 
-//const color = 
+class Word extends Component {
+    constructor(){
+        super();
+        
+        this.state = {
+            isFlipped: false,
+        }
 
-const Word = ({notation}) => { 
-    return (
-            <span>{ notation } </span>
-    );
-};
+        this.onClick = () => {
+            this.setState(
+                {
+                isFlipped: true,
+                }
+            );
+        }
+    }
+
+    
+render () {
+    
+    const { notation } = this.props;
+    const { isFlipped } = this.state;
+    let classNames = "word-card";
+    if (isFlipped) {
+        classNames+='flipped-card';
+    }
+
+    return ( 
+        <span className={classNames}
+        onClick={this.onClick} 
+        >{ notation } 
+        </span>
+    )
+    }
+
+
+}
+
+
 
 export default Word
