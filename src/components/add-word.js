8300579485c6db288923becb98fa6543d99dd0a1
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-06-21 22:55:19 +0300

    added buttons to determine the language of word added

--- src/components/add-word.js
+++ src/components/add-word.js
@@ -38,6 +38,12 @@ class NewWord extends Component  {
             <button className = "button-add"
             onClick = { () => this.props.addWord}
             >Add word</button>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-primary active" />
+            <input type="radio" name="german" id="option1" autocomplete="off" checked value /> German  
+            <label class="btn btn-primary" />
+            <input type="radio" name="english" id="option2" autocomplete="off" /> English
+            </div>
             </form>
             <form className = "item-add-form d-flex"> 
             <input 
